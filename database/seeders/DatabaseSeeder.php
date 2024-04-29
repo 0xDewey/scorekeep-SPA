a7baf5ae2c3c37739cc0d032fec0fafddb8842ca
Bastien DA SILVA <bastds.dev@gmail.com>
2024-04-29 14:27:23 +0200

    Update real seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,35 +18,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        $localTeam = LocalTeam::factory()->create([
-            'name' => "Avon Handball",
-            'logo' => "https://avonhandball.fr/wp-content/uploads/2020/08/avon-handball-1.png",
-            'token' => 77210
-        ]);
-
         $role = Role::create(['name' => 'user']);
-
-        $user = \App\Models\User::factory()->create([
-            'localTeamId' => $localTeam->uuid,
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-        ]);
-
-        $user->assignRole($role);
-
         $role = Role::create(['name' => 'admin']);
-
-        $user = \App\Models\User::factory()->create([
-            'name' => 'Test Admin',
-            'email' => 'admin@example.com',
-            'password' => 'password',
-        ]);
-
-        $user->assignRole($role);
-
         $role = Role::create(['name' => 'SA']);
 
         $user = \App\Models\User::factory()->create([
@@ -56,35 +29,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $user->assignRole($role);
-
-        $visitorTeams = VisitorTeam::factory(5)->create();
-
-        Game::factory(2)->create([
-            'localTeamId' => $localTeam->uuid,
-            'visitorTeamId' => $visitorTeams[0]->uuid,
-        ]);
-
-        Game::factory(2)->create([
-            'localTeamId' => $localTeam->uuid,
-            'visitorTeamId' => $visitorTeams[1]->uuid,
-        ]);
-
-        Game::factory(2)->create([
-            'localTeamId' => $localTeam->uuid,
-            'visitorTeamId' => $visitorTeams[2]->uuid,
-        ]);
-
-        Game::factory(2)->create([
-            'localTeamId' => $localTeam->uuid,
-            'visitorTeamId' => $visitorTeams[3]->uuid,
-        ]);
-
-        Game::factory(2)->create([
-            'localTeamId' => $localTeam->uuid,
-            'visitorTeamId' => $visitorTeams[4]->uuid,
-        ]);
-
-        // Volunteer::factory(15)->create();
 
         VolunteerType::factory()->create(['label' => "Secrétaire"]);
         VolunteerType::factory()->create(['label' => "Chronométreur"]);
